62a4d16ff1e09dc4755ecc4c34027443ce6fe16d
Navneet Prajapati <navneetprajapati26@gmail.com>
2023-11-09 13:30:05 +0530

    for map

--- lib/dart_basics.dart
+++ lib/dart_basics.dart
@@ -1,25 +1,49 @@
+import 'dart:convert';
+
 void main() {
-  Map<String, int> ageMap;
-  ageMap = {
-    'Anuja': 25,
-    'Binod': 30,
-    'Chirag': 35,
-  };
+  // Map<String, dynamic> myMap = {
+  //   'name': 'Anuja',
+  //   'age': 30,
+  //   'city': 'Varanasi',
+  // };
+  //
+  // String jsonStr = json.encode(myMap);
+  // print(myMap);
+  // print(jsonStr);
+
+  String jsonString = '''
+{
+  "name": "Chirag",
+  "address": {
+    "street": "Jhun Jhun Gali",
+    "city": "Varanasi"
+  }
+}
+''';
+  Map<dynamic, dynamic> myMap = json.decode(jsonString);
+  print(myMap["address"]["street"]);
+
+  // Map<String, int> ageMap;
+  // ageMap = {
+  //   'Anuja': 25,
+  //   'Binod': 30,
+  //   'Chirag': 35,
+  // };
 
   // for(var name in ageMap.keys){
   //   print("${name} and age ${ageMap[name]}");
   // }
-  Map<String, int> additionalAges = {'Dhannjaya': 28, 'Chotu': 29};
+  // Map<String, int> additionalAges = {'Dhannjaya': 28, 'Chotu': 29};
 
   //ageMap.addAll(additionalAges);
 
-  List<String> key = ageMap.keys.toList();
-  List<int> value = ageMap.values.toList();
-
-  print(key);
-  print(value);
-
-
+  // List<String> key = ageMap.keys.toList();
+  // List<int> value = ageMap.values.toList();
+  //
+  // print(key);
+  // print(value);
+  // ageMap.clear();
+  // print(ageMap);
 
   // ageMap.forEach((key, value) {
   //   print("${key} and ${value}");
@@ -32,5 +56,5 @@ void main() {
   // bool hasKey = ageMap.containsKey( 'Chirag');
   // print(hasKey);
 
- // print(mapSize);
+  // print(mapSize);
 }
